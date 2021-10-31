7a2f06cc109352ef98ae794067c64ce63584fdd9
[Digvikadam] <[digvikadam168@gmail.com]>
2021-10-31 17:47:36 +0530

    Added: Welcome Massage

---
+++ com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+
+        System.out.println("Welcome to Snake And Ladder Program...");
+
+        int position = 0;
+        System.out.println("Player is at Position: "+position);
+    }
+}
+
